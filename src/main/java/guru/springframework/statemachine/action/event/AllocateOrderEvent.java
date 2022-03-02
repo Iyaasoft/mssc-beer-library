afcce74934cdbd031a10cf08d6ccd63c34ad569c
to20291063 <tod.barnett@wipro.com>
2022-03-02 09:23:22 +0000

    assignment fix to AllocateOrderEvent missing annotations

--- src/main/java/guru/springframework/statemachine/action/event/AllocateOrderEvent.java
+++ src/main/java/guru/springframework/statemachine/action/event/AllocateOrderEvent.java
@@ -1,12 +1,16 @@
 package guru.springframework.statemachine.action.event;
 
 import guru.springframework.web.model.BeerOrderDto;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Builder
 @EqualsAndHashCode
